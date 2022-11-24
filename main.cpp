7baec85cb583e70ba4a28d2ac8f20064f2cdefc9
lkrot <llrot@list.ru>
2022-11-24 20:48:44 +0700

    + fast pow functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 double my_pow(double, unsigned int);
+double binary_powe(double, unsigned int);
+double fast_frac_pow1(double, double);
+double fast_frac_pow2(double, double);
 
-int main() 
+int main()
 {
 	return 0;
 }
@@ -12,3 +15,37 @@ double my_pow(double x, unsigned int p)
 		result*=x;
 	return result;
 }
+
+double binary_pow(double x, unsigned int p)
+{
+	double result=1.0;
+	while (p!=0) {
+		if ((p & 1) != 0)
+			result*=x;
+		x*=x;
+		p>>=1;
+	}
+	return result;
+}
+
+double fast_frac_pow1(double x, double p)
+{
+	union {
+		double d;
+		long long i;
+	} u = { x };
+	u.i = (long long)(4606853616395542500L + p * (u.i - 4606853616395542500L));
+	return u.d;
+}
+
+double fast_frac_pow2(double x, double p)
+{
+	union {
+		double d;
+		int i[2];
+	} u = { x };
+	u.i[1] = (int)(p * (u.i[1] - 1072632447) + 1072632447);
+	u.i[0] = 0;
+	return u.d;
+}
+

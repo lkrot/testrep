46b6c437de14966c31391de15b7b9da917969ab5
lkrot <llrot@list.ru>
2022-11-24 20:58:06 +0700

    fix negative exponents

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
-double my_pow(double, unsigned int);
+double my_pow(double, int);
 
 int main() 
 {
 	return 0;
 }
 
-double my_pow(double x, unsigned int p)
+double my_pow(double x, int p)
 {
-	double result=x;
-	for (unsigned int i=0; i<p; i++)
-		result*=x;
+	double result=1.0;
+	if (p<0) {
+		for (int i=0; i>p; i--)
+			result/=x;
+	} else {
+		for (int i=0; i<=p; i++)
+			result*=x;
+	}
 	return result;
 }
